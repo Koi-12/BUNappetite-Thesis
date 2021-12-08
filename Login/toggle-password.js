005b087a12e1973fd9f94802d9a6bb29528a50b9
Koi-12 <michaelryan.bajar12@gmail.com>
2021-12-08 18:02:38 +0800

    BUNappetite Project

--- Login/toggle-password.js
+++ Login/toggle-password.js
@@ -1,31 +1,11 @@
-function change()
-         {
-            var x = document.getElementById('pass').type;
 
-            if (x == 'password')
-            {
-               document.getElementById('pass').type = 'text';
-               document.getElementById('mybutton').innerHTML = '<i class="fas fa-eye"></i>';
-            }
-            else
-            {
-               document.getElementById('pass').type = 'password';
-               document.getElementById('mybutton').innerHTML = '<i class="fas fa-eye-slash"></i>';
-            }
-         }
-
-function change()
-         {
-            var x = document.getElementById('pass2').type;
-
-            if (x == 'password')
-            {
-               document.getElementById('pass2').type = 'text';
-               document.getElementById('mybutton2').innerHTML = '<i class="far fa-eye"></i>';
-            }
-            else
-            {
-               document.getElementById('pass2').type = 'password';
-               document.getElementById('mybutton2').innerHTML = '<i class="far fa-eye-slash"></i>';
-            }
-         }         
+function showPwd(id, el) {
+   let x = document.getElementById(id);
+   if (x.type === "password") {
+     x.type = "text";
+     el.className = 'fa fa-eye showpwd';
+   } else {
+     x.type = "password";
+     el.className = 'fa fa-eye-slash showpwd';
+   }
+ } 
